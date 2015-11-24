f4f8ea36f0510ff2efe7d25c45e0746ce5f52de0
Phill Conrad <pconrad@cs.ucsb.edu>
2015-11-24 15:24:03 -0800

    Create student1.cpp

---
+++ student_files/student1.cpp
@@ -0,0 +1,6 @@
+// REFERENCE IMPLEMENTATION of what students should write
+
+#include <iostream>
+void student1Func() {
+  std::cout << "Hello, Anacapa!  This is the expected output." << std::endl;
+}
