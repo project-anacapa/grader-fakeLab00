1e8f5be1bb4a42662461b59fcdc15c1e7f64b52c
Phill Conrad <pconrad@cs.ucsb.edu>
2015-11-30 15:08:40 -0800

    Update MyFirstApp.java

--- student_files/MyFirstApp.java
+++ student_files/MyFirstApp.java
@@ -2,6 +2,6 @@
 public class MyFirstApp {
     public static void main (String[] args) {
         System.out.println("I Rule!");
-        System.out.println("The World");
+        System.out.println("CS56");
     }   
 }
